f3391f6fee4f7dd4c9884cf455c23a1f1271847f
vedant234 <74408696+vedant234@users.noreply.github.com>
2020-11-30 16:55:25 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+// step 1- create the variable
+var box1,box2;
+var ground;
+
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+// step2 - call the constructor
+
+    box1 = new Box(200,100,50,50);
+   box2 = new Box(210,50,50,100);
+   // box2 = new Box(240,100,50,100);
+    ground = new Ground(200,height,400,20);
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+  //  console.log(box2.body.position.x);
+  //  console.log(box2.body.position.y);
+  //  console.log(box2.body.angle);
+    box1.display();
+    box2.display();
+    ground.display();
+}
